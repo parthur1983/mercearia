f400c188e62b63eee3df3db0c683d4e1e9989949
paulo.santana <paulo.santana@mpba.mp.br>
2020-03-12 10:03:27 -0300

    repositorio compartilhado

--- Nucleo/Repository/UsuarioRepository.cs
+++ Nucleo/Repository/UsuarioRepository.cs
@@ -10,45 +10,12 @@ using System.Threading.Tasks;
 
 namespace Nucleo.Repository
 {
-    public interface IUsuarioRepository
+    public interface IUsuarioRepository : ICrudRepository<Usuario>
     {
-        void Salvar(Usuario usuario);
-        void Excluir(Usuario usuario);
-        Usuario ObterPor(int id);
-        List<Usuario> ObterTodos();
     }
 
-    public class UsuarioRepository : IUsuarioRepository
+    public class UsuarioRepository : CrudRepository<Usuario>, IUsuarioRepository
     {
-
-       private readonly ISession _session = new HibernateSession().OpenSession();
-
-        public void Salvar(Usuario usuario)
-        {
-            using (var tran = _session.BeginTransaction())
-            {
-                _session.SaveOrUpdate(usuario);
-                tran.Commit();
-            }
-        }
-
-        public void Excluir(Usuario usuario)
-        {
-            using (var tran = _session.BeginTransaction())
-            {
-                _session.Delete(usuario);
-                tran.Commit();
-            }
-        }
-
-        public Usuario ObterPor(int id)
-        {
-            return _session.Get<Usuario>(id);
-        }
-
-        public List<Usuario> ObterTodos()
-        {
-            return _session.Query<Usuario>().ToList();
-        }
+       
     }
 }
